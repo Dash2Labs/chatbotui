605145e641d2c933010c4ce1e34fa77e54ba0c14
Dustin <dustinnuc@gmail.com>
2024-08-18 17:06:05 -0700

    resolved some issues with the base project and added bubble

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import { standardTheme } from './components/theme_provider.ts';
+import Bubble from './components/bubble.tsx';
 const App = () => {
-    return <></>;
+    return <Bubble theme={standardTheme}
+                   margin='0px'
+                   borderRadius='5px'
+                   padding='5px'/>;
 }
 
 export default App;
